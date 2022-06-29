bad2e28e712bd4e68d1efd13e99d8e59da2316e1
Yannis699 <yannis.cristini@gmail.com>
2022-06-29 19:03:19 +0200

    Sidebar

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
@@ -12,3 +13,27 @@ import './styles/app.scss';
 import './bootstrap';
 
 require('bootstrap');
+require("@fortawesome/fontawesome-free/css/all.min.css");
+require("@fortawesome/fontawesome-free/js/all.js");
+
+window.onload = () => {
+    /*====== Sidebar admin ======*/
+
+    let aside = document.querySelector(".sidebar_admin");
+    let icon = aside.querySelector(".menu_icon");
+    let li = aside.querySelectorAll(".side_item");
+
+    icon.onclick = () => {
+        aside.classList.toggle("expand");
+    };
+
+    for (let i of li) {
+        i.onclick = activeLi;
+    }
+
+    function activeLi() {
+        const list = Array.from(li);
+        list.forEach((e) => e.classList.remove("active"));
+        this.classList.add("active");
+    }
+}
