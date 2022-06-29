842c65f6d6b7c48514021c484d6233b79af749e4
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-29 16:42:58 +0200

    Creation of project controller

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/project', name: 'app_project')]
+    public function index(): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    #[Route('/project/list', name: 'app_project_list')]
+    public function list(): Response
+    {
+        return $this->render('project/list.html.twig');
+    }
+}
